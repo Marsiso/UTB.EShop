43deb887dfe8d1b0228466e2b3cee3e01dad6ca8
Marek Olsak <m_olsak@utb.cz>
2022-11-26 00:38:07 +0100

    Infrastructure layer added.

---
+++ UTB.EShop.DistributedServices.WebAPI/Controllers/CarouselController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UTB.EShop.DistributedServices.WebAPI.Controllers;
+
+[ApiController]
+[Route("Carousel")]
+public class CarouselController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return Ok();
+    }
+}
